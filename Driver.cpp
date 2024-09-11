170925014aeed12e7e2c7ce5635f35cc560796c3
jdieter123 <jdieter16@gmail.com>
2024-09-11 16:14:37 -0500

    Very basic menu for driver

--- Driver.cpp
+++ Driver.cpp
@@ -1,12 +1,39 @@
 
 
-
+#include <iostream>
 using namespace std;
+
+//function prototypes
+int DisplayMenuGetChoice();
 
 int main ()
 {
+    int choice;
 
+    do {
+        choice = DisplayMenuGetChoice();
+
+        switch(choice) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+        }
+    } while(choice != 4);
 
 
     return 0;
+}
+
+int DisplayMenuGetChoice() {
+    int choice;
+
+    cout << "WELCOME MESSAGE"
+		 << "1 - Display\n"
+		 << "2 - Add\n"
+		 << "3 - Remove\n"
+         << "4 - End the Program\n"
+		 << "\nCHOICE: ";
 }
