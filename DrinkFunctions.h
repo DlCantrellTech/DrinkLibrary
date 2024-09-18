f8dddd60adc29cee81970212ca1ec5d11479982e
jdieter123 <jdieter16@gmail.com>
2024-09-18 15:20:53 -0500

    added class specifications

--- DrinkFunctions.h
+++ DrinkFunctions.h
@@ -1,2 +1,16 @@
+//class specification file
+#ifndef DRINKFUNCTIONS_H
+#define DRINKFUNCTIONS_H
+
+#include <iostream>
+
+using namespace std;
+
+class DRINKFUNCTIONS {
+    
+};
+
 //function prototypes
 int DisplayMenuGetChoice();
+
+#endif
