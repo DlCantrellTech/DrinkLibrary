cc41108c13ac8f10bd6058f2dbdabfa6b035c456
TheWildYasuo <dlcantrell42@tntech.edu>
2024-09-20 17:49:18 -0500

    Added Data Class files

---
+++ Data.h
@@ -0,0 +1,19 @@
+/*
+    Title: Data.h
+    Project 1 - CSC1310
+    Authors: Levi, Brody, Ryan, Justin
+    Date: 9/18/2024
+ */
+
+#ifndef Data_H
+#define Data_H
+
+class Data {
+    private:
+       void getData();
+
+};
+
+
+
+#endif
